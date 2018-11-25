086ea28ff2ff786c041ec005a9c1cff9903da9e3
Víctor García <gc.victor@gmail.com>
2018-11-25 23:58:44 +0100

    feat(example) update example with defatul value case

--- example/index.js
+++ example/index.js
@@ -1,35 +1,63 @@
-import React, { InputHTMLAttributes } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import { Form, Field, ErrorMessage, FormConsumer } from '../dist';
 
-const validator = (value) => (value ? 'Error: Error Message!' : '');
+const validator = value => (value ? 'Error: Error Message!' : '');
 const Input = ({ name = '', ...rest }) =>
     <Field name={name} {...rest}>
         <input {...rest} />
     </Field>;
+const Select = ({ name = '', ...rest }) =>
+    <Field name={name} {...rest}>
+        <select {...rest}>
+            <option value="0">0</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+        </select>
+    </Field>;
 const Message = ({ name }) =>
     <FormConsumer>
-        {({ errors }) => {
-            const message = errors && errors[name];
+        {({ errors, values }) => {
+            const errorMessage = errors && errors[name];
+            const message = values && values[name];
 
-            return (
-                message &&
-                <p>
-                    {errors[name]}
-                </p>
-            );
+            return <React.Fragment>
+                {
+                    errorMessage &&
+                    <p>
+                        {errorMessage}
+                    </p>
+                }
+                {
+                    message &&
+                    <p>
+                        {message}
+                    </p>
+                }
+            </React.Fragment>;
         }}
     </FormConsumer>;
 
 ReactDOM.render(
     <Form>
-        <label>
-            <span>Error Message</span>
-            <ErrorMessage name={'error'} validator={validator}>
-                <Input id={'error'} name={'error'} />
-            </ErrorMessage>
-        </label>
-        <Message name={'error'} />
+        <div>
+            <label>
+                <span>Error Message</span>
+                <ErrorMessage name={'input'} validator={validator}>
+                    <Input id={'input'} name={'input'} defaultValue={'test'} />
+                </ErrorMessage>
+            </label>
+            <Message name={'input'} />
+        </div>
+        <div>
+            <label>
+                <span>Select</span>
+                <ErrorMessage name={'select'} validator={validator}>
+                    <Select id={'select'} name={'select'} defaultValue={'1'} />
+                </ErrorMessage>
+            </label>
+            <Message name={'select'} />
+        </div>
     </Form>,
     document.getElementById('root')
 );
