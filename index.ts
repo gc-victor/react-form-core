a6e3246f53af1153ea0f9f63ae64f54bfe4fbcde
Víctor García <gc.victor@gmail.com>
2018-11-26 00:24:07 +0100

    feat(index) update exports

--- index.ts
+++ index.ts
@@ -1,6 +1,3 @@
-export { ErrorMessage } from './src/error-message/error-message.element';
-export { withErrorMessage } from './src/error-message/error-message.with-error-message';
-export { Field } from './src/field/field.element';
-export { Form } from './src/form/form.component';
-export { FormConsumer } from './src/form/form.consumer';
-export { FormContext } from './src/form/form.context';
+export * from './src/error-message';
+export * from './src/field';
+export * from './src/form';
